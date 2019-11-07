1dc5b90deabc448006534bb867a139ccfd849b2c
Sagha <Sagha@LAPTOP-AUS1OPRL>
2019-11-08 00:24:21 +0400

    ChangedMatrix

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -4,12 +4,55 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Homework1
+namespace ConsoleApp16
 {
-    class Program
+    class ChangedMatrixx
     {
+
+        public static void ChangedMatrix(int n, int[,] A)
+        {
+
+            Console.WriteLine("Changed matrix");
+
+            int k = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < k; j++)
+                {
+                    A[i, j] = 0;
+                }
+                k++;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(A[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.WriteLine("Enter square matrix size ");
+                int n = Convert.ToInt32(Console.ReadLine());
+                int[,] A = new int[n, n];
+                Random r = new Random();
+                for (int i = 0; i < n; i++)
+                {
+                    for (int j = 0; j < n; j++)
+                    {
+                        A[i, j] = r.Next(1, 9);
+                        Console.Write(A[i, j] + " ");
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+                ChangedMatrix(n, A);
+            }
         }
     }
 }
+
